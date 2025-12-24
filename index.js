b987527ad5c0c49ea427d05b9aab868846184778
kathleenlohanny433@gmail.com <kathleenlohanny433@gmail.com>
2025-12-24 15:24:30 -0800

    terceiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque = "";
+
+    if (this.tipo === "mago") {
+      ataque = "usou magia";
+    } else if (this.tipo === "guerreiro") {
+      ataque = "usou espada";
+    } else if (this.tipo === "monge") {
+      ataque = "usou artes marciais";
+    } else if (this.tipo === "ninja") {
+      ataque = "usou shuriken";
+    }
+
+    console.log(`o ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+// Exemplo de uso
+let heroi = new Heroi("Artemis", 25, "mago");
+heroi.atacar();
